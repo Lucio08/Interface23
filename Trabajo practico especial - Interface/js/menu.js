d6b399ed8df74a65cb22d8b3469b709adbdcfeca
Lucio Ingargiola <lucioingargiola@hotmail.com>
2023-10-09 20:19:10 -0300

    avance entregable

--- Trabajo practico especial - Interface/js/menu.js
+++ Trabajo practico especial - Interface/js/menu.js
@@ -1,6 +1,9 @@
-document.querySelector(".btn-menu-svg").addEventListener("click", toggleMenu);
-
+document.querySelector(".btn-menu").addEventListener("click", toggleMenu);
 function toggleMenu() {
     document.querySelector(".nav").classList.toggle("show");
 }
  
+document.querySelector(".btn-usuario").addEventListener("click", toggleMenuUsuario);
+function toggleMenuUsuario() {
+    document.querySelector(".nav-usuario").classList.toggle("show");
+}
