dc4ff567470bad2f92ea9afd0022200454e6da8e
Lucio Ingargiola <lucioingargiola@hotmail.com>
2023-10-09 21:11:37 -0300

    Cambios

--- Trabajo practico especial - Interface/js/carga.js
+++ Trabajo practico especial - Interface/js/carga.js
@@ -1,21 +1,29 @@
-var text = document.querySelector('.text');
-var percentaje = document.querySelector('.percentaje');
-var progreso = document.querySelector('.progreso');
-var count = 4;
-var per = 16;
-var loading = setInterval(animate, 50);
-function animate(){
-  if(count == 100 && per == 400){
-    text.textContent = "Completado";
-    text.style.fontSize = "70px";
-    text.classList.add("add");
-    clearInterval(loading);
-    // Redirigir a la página de inicio 
-    window.location.href = "index.html";
-  }else{
-    per = per + 4;
-    count = count + 1;
-    progreso.style.width = per + 'px';
-    percentaje.textContent = count + '%';
+document.getElementById('startAnimation').addEventListener('click', function(event) {
+  event.preventDefault(); // Prevenir el envío del formulario
+
+  var loadingContainer = document.getElementById('loadingContainer');
+  loadingContainer.style.display = 'block';
+
+  var text = document.querySelector('.text');
+  var percentaje = document.querySelector('.percentaje');
+  var progreso = document.querySelector('.progreso');
+  var count = 0;
+  var per = 0;
+  var loading = setInterval(animate, 50);
+
+  function animate() {
+      if (count == 100 && per == 400) {
+          text.textContent = "Completado";
+          text.style.fontSize = "70px";
+          text.classList.add("add");
+          clearInterval(loading);
+          // Redirigir a la página de inicio 
+          window.location.href = "index.html";
+      } else {
+          per = per + 4;
+          count = count + 1;
+          progreso.style.width = per + 'px';
+          percentaje.textContent = count + '%';
+      }
   }
-}
+});
